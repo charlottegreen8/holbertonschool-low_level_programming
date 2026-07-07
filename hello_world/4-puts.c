a198fb0b0cf2dd6170699709e35ba23e69efd608
charlottegreen8 <14823@holbertonstudents.com>
2026-07-07 12:21:41 +1000

    Task 4 attempt 2

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 
-/** 
- * main - entry point
- * 
- * Return: Always 0 (Success)
- */
-int main(void)
+/**
+* main - entry point
+*
+* Return: Always 0 (Success)
+*/
+intmain(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+return (0);
 }
