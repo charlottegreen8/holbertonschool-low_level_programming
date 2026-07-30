8c04404eeea6949989892f2c178bfc46cc172b8a
charlottegreen8 <14823@holbertonstudents.com>
2026-07-30 11:39:25 +1000

    Project 2 submission

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h> 
+#include <stdlib.h>
+/**
+ * 
+ * main - Entry point 
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char n; 
+
+	n = 'a';
+
+	while (n >= 'a' && n <= 'z')		
+	{			
+		putchar(n);
+		n++;
+	}
+
+	putchar('\n');
+
+	return (0); 
+}
