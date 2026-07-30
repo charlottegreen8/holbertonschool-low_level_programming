86093453dd7bc75c6e6a43ec3dca2e3d2b5cafa5
charlottegreen8 <14823@holbertonstudents.com>
2026-07-30 11:41:14 +1000

    Project 2 submission

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,24 +1,24 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
 /**
- * 
- * main - Entry point 
+ *
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	char n; 
+	char n;
 
 	n = 'a';
 
-	while (n >= 'a' && n <= 'z')		
-	{			
+	while (n >= 'a' && n <= 'z')
+	{
 		putchar(n);
 		n++;
 	}
 
 	putchar('\n');
 
-	return (0); 
+	return (0);
 }
