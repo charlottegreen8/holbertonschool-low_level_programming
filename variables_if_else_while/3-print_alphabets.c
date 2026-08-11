1ab59a8613e520000d63548abdf971b96c8bcf5a
charlottegreen8 <14823@holbertonstudents.com>
2026-08-11 21:02:44 +1000

    Task 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        char n;
+	char o;
+
+        n = 'a';
+	o = 'A';
+
+	while (n >= 'a' && n <= 'z')
+        {       
+        putchar(n);
+        n++;    
+        }   
+
+        while (o >= 'A' && o <= 'Z')
+	{
+        putchar(o);
+        o++;
+        }	
+
+	putchar('\n');
+        return (0);
+}
