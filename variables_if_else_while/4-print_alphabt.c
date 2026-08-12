ccbed9fc7c9f5a47ccb445f29d17fbfe74ca0c71
charlottegreen8 <14823@holbertonstudents.com>
2026-08-12 13:21:12 +1000

    Added the executable file last time not the C file

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char n;
+
+	n = 'a';
+
+	while (n >= 'a' && n <= 'z')
+	{
+		if (n != 'e' && n != 'q')
+			{
+			putchar(n);
+			}
+	n++;
+	}
+
+	putchar('\n');
+	return (0);
+}
