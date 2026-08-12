12ec9f80d6324d3421f3f472b134567257ad0cfc
charlottegreen8 <14823@holbertonstudents.com>
2026-08-12 11:45:37 +1000

    Adjusted spaces to tabs

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - Entry point
- * Return: Always 0 (Success)
- */
+* main - Entry point
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-        char n;
+	char n;
 	char o;
 
-        n = 'a';
+	n = 'a';
 	o = 'A';
 
 	while (n >= 'a' && n <= 'z')
-        {       
-        putchar(n);
-        n++;    
-        }   
-
-        while (o >= 'A' && o <= 'Z')
 	{
-        putchar(o);
-        o++;
-        }	
+	putchar(n);
+	n++;
+	}
+
+	while (o >= 'A' && o <= 'Z')
+	{
+	putchar(o);
+	o++;
+	}
 
 	putchar('\n');
-        return (0);
+	return (0);
 }
