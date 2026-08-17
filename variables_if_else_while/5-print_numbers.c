a7627790196e1bcd81eb464cb2e770adfa319eda
charlottegreen8 <14823@holbertonstudents.com>
2026-08-17 21:29:02 +1000

    Task 5 Complete. Prints single digit numbers 0-9

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,22 @@
-TEST
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+
+	putchar('\n');
+	return (0);
+}
